12bc5521742ef0cd845ecf735913903cd24f83d9
MilesCranmer <miles.cranmer@gmail.com>
2020-09-15 08:34:20 -0400

    Benchmark median time

--- benchmark.jl
+++ benchmark.jl
@@ -1,16 +1,12 @@
 include("paralleleureqa.jl")
-fullRun(1,
-    npop=100,
-    annealing=true,
-    ncyclesperiteration=1000,
-    fractionReplaced=0.1f0,
-    verbosity=0)
-@time for i=1:5
-    fullRun(3,
+using BenchmarkTools
+
+t = @benchmark(fullRun(3,
         npop=100,
         annealing=true,
         ncyclesperiteration=100,
         fractionReplaced=0.1f0,
         verbosity=0
-    )
-end
+       ), evals=5)
+
+println("The median time is $(median(t))")
