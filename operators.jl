5fcecfa34ffbf1f9fe8d378513695ec051af1918
MilesCranmer <miles.cranmer@gmail.com>
2020-09-16 11:04:05 -0400

    Add operator for safe power

--- operators.jl
+++ operators.jl
@@ -1,3 +1,4 @@
-# Define allowed operators
+# Define allowed operators. Any julia operator can also be used.
 plus(x::Float32, y::Float32)::Float32 = x+y
 mult(x::Float32, y::Float32)::Float32 = x*y;
+pow(x::Float32, y::Float32)::Float32 = sign(x)*abs(x)^y;
