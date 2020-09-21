470f7688ce994c7928b0b193162842b4e5021229
MilesCranmer <miles.cranmer@gmail.com>
2020-09-21 00:36:27 -0400

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysr", # Replace with your own username
-    version="0.1.0",
+    version="0.2.0",
     author="Miles Cranmer",
     author_email="miles.cranmer@gmail.com",
     description="Simple and efficient symbolic regression",
