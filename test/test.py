ddb4d5281816d07ef88d2acc0e2cf09911379889
Miles Cranmer <miles.cranmer@gmail.com>
2020-09-26 16:53:23 -0400

    Add test for custom operator

--- test/test.py
+++ test/test.py
@@ -3,5 +3,16 @@ from pysr import pysr
 X = np.random.randn(100, 5)
 y = X[:, 0]
 
-equations = pysr(X, y, niterations=2)
+equations = pysr(X, y,
+                 niterations=100)
 print(equations)
+# Accuracy assertion
+assert equations.iloc[-1]['MSE'] < 1e-10
+
+y = X[:, 0]**2
+equations = pysr(X, y,
+                 unary_operators=["square(x) = x^2"], binary_operators=["plus"],
+                 niterations=100)
+print(equations)
+# Accuracy assertion
+assert equations.iloc[-1]['MSE'] < 1e-10
