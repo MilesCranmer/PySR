bdc71b3ec3699686ecf0189edf291de683126097
MilesCranmer <miles.cranmer@gmail.com>
2020-09-26 21:18:28 -0400

    Clean up travis code

--- test/travis.sh
+++ test/travis.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-julia -e 'import Pkg; Pkg.add("Optim"); Pkg.add("SpecialFunctions")'
-sudo python3 -m pip install numpy pandas
-sudo python3 setup.py install
-python3 test/test.py
+sudo python3 -m pip install numpy pandas &&
+    sudo python3 setup.py install &&
+    python3 test/test.py
 
