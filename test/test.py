48b723d481ee030d1b1a6ab412d4d0ac0bcf01fd
MilesCranmer <miles.cranmer@gmail.com>
2020-09-27 20:29:33 -0400

    Add test for empty unary_operators list

--- test/test.py
+++ test/test.py
@@ -2,18 +2,27 @@ import numpy as np
 from pysr import pysr
 X = np.random.randn(100, 5)
 
-# Test 1
+print("Test 1 - defaults; simple linear relation")
 y = X[:, 0]
 equations = pysr(X, y,
-                 niterations=100)
+                 niterations=10)
 print(equations)
 assert equations.iloc[-1]['MSE'] < 1e-10
 
-# Test 2
+print("Test 2 - test custom operator")
 y = X[:, 0]**2
 equations = pysr(X, y,
                  unary_operators=["square(x) = x^2"], binary_operators=["plus"],
-                 niterations=100)
+                 niterations=10)
 print(equations)
 assert equations.iloc[-1]['MSE'] < 1e-10
 
+X = np.random.randn(100, 1)
+y = X[:, 0] + 3.0
+print("Test 3 - empty operator list, and single dimension input")
+equations = pysr(X, y,
+                 unary_operators=[], binary_operators=["plus"],
+                 niterations=10)
+
+print(equations)
+assert equations.iloc[-1]['MSE'] < 1e-10
