14f69cfc0b80ddf4a908641dff90c0ab44d66ef5
MilesCranmer <miles.cranmer@gmail.com>
2020-10-11 03:40:37 -0400

    Make fast power portable

--- julia/operators.jl
+++ julia/operators.jl
@@ -4,7 +4,8 @@ import SpecialFunctions: gamma, lgamma, erf, erfc, beta
 import Base.FastMath: sqrt_llvm_fast, neg_float_fast,
     add_float_fast, sub_float_fast, mul_float_fast, div_float_fast, rem_float_fast,
     eq_float_fast, ne_float_fast, lt_float_fast, le_float_fast,
-    sign_fast, abs_fast, log_fast, log2_fast, log10_fast, sqrt_fast
+    sign_fast, abs_fast, log_fast, log2_fast, log10_fast, sqrt_fast,
+    pow_fast
 
 # Implicitly defined:
 #binary: mod
@@ -15,7 +16,7 @@ import Base.FastMath: sqrt_llvm_fast, neg_float_fast,
 plus(x::Float32, y::Float32)::Float32 = add_float_fast(x, y) #Do not change the name of this operator.
 sub(x::Float32, y::Float32)::Float32 = sub_float_fast(x, y) #Do not change the name of this operator.
 mult(x::Float32, y::Float32)::Float32 = mul_float_fast(x, y) #Do not change the name of this operator.
-pow(x::Float32, y::Float32)::Float32 = sign_fast(x)*ccall(("powf",libm), Float32, (Float32,Float32), abs_fast(x), y)
+pow(x::Float32, y::Float32)::Float32 = sign_fast(x)*pow_fast(abs(x), y)
 div(x::Float32, y::Float32)::Float32 = div_float_fast(x, y)
 logm(x::Float32)::Float32 = log_fast(abs_fast(x) + 1f-8)
 logm2(x::Float32)::Float32 = log2_fast(abs_fast(x) + 1f-8)
