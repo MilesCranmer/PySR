9df0659591ced9ea8cdeb3d1c82d01dd0eb029ae
MilesCranmer <miles.cranmer@gmail.com>
2020-10-13 05:51:24 -0400

    Iterate version - operator constraints

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysr", # Replace with your own username
-    version="0.3.24",
+    version="0.3.25",
     author="Miles Cranmer",
     author_email="miles.cranmer@gmail.com",
     description="Simple and efficient symbolic regression",
