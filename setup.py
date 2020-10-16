89cf5864a352ee302775c8a949bdfab9f55f32e6
MilesCranmer <miles.cranmer@gmail.com>
2020-10-16 17:23:16 -0400

    Fix negative operator - used wrong fast function

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysr", # Replace with your own username
-    version="0.3.26",
+    version="0.3.27",
     author="Miles Cranmer",
     author_email="miles.cranmer@gmail.com",
     description="Simple and efficient symbolic regression",
