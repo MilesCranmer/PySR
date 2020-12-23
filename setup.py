b463028ef0f6b6acf0e21b726f261d9dde2ca81b
MilesCranmer <miles.cranmer@gmail.com>
2020-12-22 19:46:59 -0500

    Fix date formatting for windows

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysr", # Replace with your own username
-    version="0.3.33",
+    version="0.3.34",
     author="Miles Cranmer",
     author_email="miles.cranmer@gmail.com",
     description="Simple and efficient symbolic regression",
