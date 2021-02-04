1a8a8f0c3b8dcfd3045f5dabc2694d85e78ddf88
MilesCranmer <miles.cranmer@gmail.com>
2021-02-04 17:27:51 -0500

    Enable current version of SymbolicRegression.jl

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysr", # Replace with your own username
-    version="0.4.4",
+    version="0.4.6",
     author="Miles Cranmer",
     author_email="miles.cranmer@gmail.com",
     description="Simple and efficient symbolic regression",
