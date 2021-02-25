0d95e9c652dbd1f0d247c148920045aba38bb3d4
MilesCranmer <miles.cranmer@gmail.com>
2021-02-24 22:54:37 -0500

    Bump version to latest SymbolicRegression patch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysr", # Replace with your own username
-    version="0.5.2",
+    version="0.5.3",
     author="Miles Cranmer",
     author_email="miles.cranmer@gmail.com",
     description="Simple and efficient symbolic regression",
