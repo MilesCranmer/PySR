10ff16a4ee3b3d566e01a195f5ba0ec16ac3ec46
MilesCranmer <miles.cranmer@gmail.com>
2021-04-26 03:00:34 -0400

    Use old default kwargs for test

--- test/test.py
+++ test/test.py
@@ -3,11 +3,17 @@ from pysr import pysr
 import sympy
 X = np.random.randn(100, 5)
 
+default_test_kwargs = dict(
+    niterations=10,
+    populations=4,
+    user_input=False,
+    annealing=True,
+    useFrequency=False,
+)
+
 print("Test 1 - defaults; simple linear relation")
 y = X[:, 0]
-equations = pysr(X, y,
-                 niterations=10,
-                 user_input=False)
+equations = pysr(X, y, **default_test_kwargs)
 print(equations)
 assert equations.iloc[-1]['MSE'] < 1e-4
 
@@ -16,8 +22,7 @@ y = X[:, 0]**2
 equations = pysr(X, y,
                  unary_operators=["sq(x) = x^2"], binary_operators=["plus"],
                  extra_sympy_mappings={'square': lambda x: x**2},
-                 niterations=10,
-                 user_input=False)
+                 **default_test_kwargs)
 print(equations)
 assert equations.iloc[-1]['MSE'] < 1e-4
 
@@ -26,7 +31,7 @@ y = X[:, 0] + 3.0
 print("Test 3 - empty operator list, and single dimension input")
 equations = pysr(X, y,
                  unary_operators=[], binary_operators=["plus"],
-                 niterations=10,
-                 user_input=False)
+                 **default_test_kwargs)
+
 print(equations)
 assert equations.iloc[-1]['MSE'] < 1e-4
