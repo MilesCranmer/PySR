569f4badfb7dc7be9f04bd940e05b5bba1b419fe
MilesCranmer <miles.cranmer@gmail.com>
2021-05-04 15:19:34 -0400

    Bump version to 0.6.0 with new defaults

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysr", # Replace with your own username
-    version="0.5.16-2",
+    version="0.6.0",
     author="Miles Cranmer",
     author_email="miles.cranmer@gmail.com",
     description="Simple and efficient symbolic regression",
