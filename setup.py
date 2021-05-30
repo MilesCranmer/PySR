8af311956793080d0cdde96a389d27f313d57227
MilesCranmer <miles.cranmer@gmail.com>
2021-05-30 02:03:51 -0400

    Set version to release candidate

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysr", # Replace with your own username
-    version="0.6.0",
+    version="0.6.0rc1",
     author="Miles Cranmer",
     author_email="miles.cranmer@gmail.com",
     description="Simple and efficient symbolic regression",
