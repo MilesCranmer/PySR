03f8fff24e78a8c5bc5e9e20849a3a1f731cdcb1
MilesCranmer <miles.cranmer@gmail.com>
2021-05-30 21:03:42 -0400

    Fix torch test to use numpy for assert

--- test/test_torch.py
+++ test/test_torch.py
@@ -9,4 +9,4 @@ cosx = 1.0 * sympy.cos(x) + y
 X = torch.randn((1000, 2))
 true = 1.0 * torch.cos(X[:, 0]) + X[:, 1]
 torch_module = sympy2torch(cosx, [x, y, z])
-assert jnp.all(jnp.isclose(torch_module(X), true)).item()
+assert np.all(np.isclose(torch_module(X).detach().numpy(), true.detach().numpy()))
