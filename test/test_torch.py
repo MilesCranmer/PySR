4d7fdcd978a777dfe039409393d3e6442916ab4c
MilesCranmer <miles.cranmer@gmail.com>
2021-05-30 21:29:45 -0400

    Correct number of features in test

--- test/test_torch.py
+++ test/test_torch.py
@@ -6,7 +6,7 @@ import sympy
 print("Test Torch 1 - test export")
 x, y, z = sympy.symbols('x y z')
 cosx = 1.0 * sympy.cos(x) + y
-X = torch.randn((1000, 2))
+X = torch.randn((1000, 3))
 true = 1.0 * torch.cos(X[:, 0]) + X[:, 1]
 torch_module = sympy2torch(cosx, [x, y, z])
 assert np.all(np.isclose(torch_module(X).detach().numpy(), true.detach().numpy()))
