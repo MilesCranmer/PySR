3d7c3037cd3e7942c50b38798da861123dd62be0
MilesCranmer <miles.cranmer@gmail.com>
2021-05-30 20:03:56 -0400

    Add test for pytorch export

---
+++ test/test_torch.py
@@ -0,0 +1,12 @@
+import numpy as np
+from pysr import sympy2torch
+import torch
+import sympy
+
+print("Test Torch 1 - test export")
+x, y, z = sympy.symbols('x y z')
+cosx = 1.0 * sympy.cos(x) + y
+X = torch.randn((1000, 2))
+true = 1.0 * torch.cos(X[:, 0]) + X[:, 1]
+torch_module = sympy2torch(cosx, [x, y, z])
+assert jnp.all(jnp.isclose(torch_module(X), true)).item()
