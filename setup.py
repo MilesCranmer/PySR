3bea8e394a1dc49fc51455e3cba50c2b8016d4e0
MilesCranmer <miles.cranmer@gmail.com>
2021-05-31 20:33:11 -0400

    Remove extra install

--- setup.py
+++ setup.py
@@ -1,31 +1,22 @@
-import importlib.util
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-extra_installs = []
-
-torch_installed = (importlib.util.find_spec('torch') is not None)
-install_sympytorch = torch_installed
-
-if install_sympytorch:
-    extra_installs.append('sympytorch')
-
 setuptools.setup(
-    name="pysr", # Replace with your own username
-    version="0.6.0rc2",
+    name="pysr",
+    version="0.6.0rc3",
     author="Miles Cranmer",
     author_email="miles.cranmer@gmail.com",
     description="Simple and efficient symbolic regression",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MilesCranmer/pysr",
-    install_requires=([
+    install_requires=[
         "numpy",
         "pandas",
         "sympy"
-        ] + extra_installs),
+        ],
     packages=setuptools.find_packages(),
     package_data={
         'pysr': ['../Project.toml', '../datasets/*']
