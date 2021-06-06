f09bbbc82716058c66ca7a14004635c2073375b3
MilesCranmer <miles.cranmer@gmail.com>
2021-06-06 04:30:40 -0400

    Update PySR version with Inf/Nan fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysr",
-    version="0.6.3",
+    version="0.6.4",
     author="Miles Cranmer",
     author_email="miles.cranmer@gmail.com",
     description="Simple and efficient symbolic regression",
