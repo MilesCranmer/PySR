d306cbe59e5a0ace2a048fc1359f2fcf94376b19
MilesCranmer <miles.cranmer@gmail.com>
2021-06-09 02:01:49 -0400

    Fix DaemonMode dir

--- pysr/sr.py
+++ pysr/sr.py
@@ -434,8 +434,10 @@ def pysr(
     pkg_directory = kwargs["pkg_directory"]
     if kwargs["julia_project"] is not None:
         manifest_filepath = Path(kwargs["julia_project"]) / "Manifest.toml"
+        kwargs["julia_project"] = Path(kwargs["julia_project"])
     else:
         manifest_filepath = pkg_directory / "Manifest.toml"
+        kwargs["julia_project"] = pkg_directory
 
     kwargs["need_install"] = False
 
@@ -484,7 +486,6 @@ def _start_julia_daemon(
         return
 
     s = "import Pkg;"
-    s += 'Pkg.add("DaemonMode");'
     if need_install:
         s += "Pkg.instantiate();"
         s += "Pkg.update();"
