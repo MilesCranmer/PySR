13e9c165885abeabf2b7faf23835182292adc694
MilesCranmer <miles.cranmer@gmail.com>
2021-06-14 02:07:02 -0400

    Update backend with worker scheduler

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysr",
-    version="0.6.8-1",
+    version="0.6.8-2",
     author="Miles Cranmer",
     author_email="miles.cranmer@gmail.com",
     description="Simple and efficient symbolic regression",
