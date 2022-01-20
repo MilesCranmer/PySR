16f91b68357f224b7c80e4df79e6973ff11effc0
MilesCranmer <miles.cranmer@gmail.com>
2022-01-20 14:17:48 -0500

    Add code citation

--- test/test_static_libpython_warning.py
+++ test/test_static_libpython_warning.py
@@ -3,6 +3,7 @@
 import warnings
 import pysr
 
+# Taken from https://stackoverflow.com/a/14463362/2689923
 with warnings.catch_warnings(record=True) as warning_catcher:
     warnings.simplefilter("always")
     pysr.sr.init_julia()
