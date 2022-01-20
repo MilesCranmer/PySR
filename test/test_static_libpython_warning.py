54c02c44d90a0d7bca06a82718f8d79235bd1aad
MilesCranmer <miles.cranmer@gmail.com>
2022-01-20 14:12:04 -0500

    Explicit test for user warning

---
+++ test/test_static_libpython_warning.py
@@ -0,0 +1,12 @@
+"""Test that running PySR with static libpython raises a warning."""
+
+import warnings
+import pysr
+
+with warnings.catch_warnings(record=True) as warning_catcher:
+    warnings.simplefilter("always")
+    pysr.sr.init_julia()
+
+    assert len(warning_catcher) == 1
+    assert issubclass(warning_catcher[-1].category, UserWarning)
+    assert "static" in str(warning_catcher[-1].message)
