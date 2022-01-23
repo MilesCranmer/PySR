acce2c2d322c801e4c93487b50cca00567139706
MilesCranmer <miles.cranmer@gmail.com>
2022-01-23 16:06:12 -0500

    Convert static libpython test to proper unittest

--- test/test_static_libpython_warning.py
+++ test/test_static_libpython_warning.py
@@ -1,13 +1,20 @@
-"""Test that running PySR with static libpython raises a warning."""
+"""Test that running PySR with static libpython raises a warning.
 
+Note: This test will ONLY pass with statically-linked python binaries, such
+as provided by conda. It will not pass on other versions of python, and that is
+okay."""
+
+import unittest
 import warnings
 import pysr
 
 # Taken from https://stackoverflow.com/a/14463362/2689923
-with warnings.catch_warnings(record=True) as warning_catcher:
-    warnings.simplefilter("always")
-    pysr.sr.init_julia()
+class TestLibpythonWarning(unittest.TestCase):
+    def test_warning(self):
+        with warnings.catch_warnings(record=True) as warning_catcher:
+            warnings.simplefilter("always")
+            pysr.sr.init_julia()
 
-    assert len(warning_catcher) == 1
-    assert issubclass(warning_catcher[-1].category, UserWarning)
-    assert "static" in str(warning_catcher[-1].message)
+            self.assertEqual(len(warning_catcher), 1)
+            self.assertTrue(issubclass(warning_catcher[-1].category, UserWarning))
+            self.assertIn("static", str(warning_catcher[-1].message))
