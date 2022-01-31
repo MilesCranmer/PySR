bae75dbd8315d99c010c6bc6148a269fde491d33
MilesCranmer <miles.cranmer@gmail.com>
2022-01-31 09:09:55 -0500

    Bump version to 0.7.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name="pysr",
-    version="0.7.0a3",
+    version="0.7.0",
     author="Miles Cranmer",
     author_email="miles.cranmer@gmail.com",
     description="Simple and efficient symbolic regression",
