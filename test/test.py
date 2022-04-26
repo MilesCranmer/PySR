1b17efef0b099588af6f491d92ff782ab9ddd2ce
MilesCranmer <miles.cranmer@gmail.com>
2022-04-26 16:58:44 -0400

    Test custom operator with underscore

--- test/test.py
+++ test/test.py
@@ -39,8 +39,8 @@ class TestPipeline(unittest.TestCase):
     def test_multioutput_custom_operator_quiet(self):
         y = self.X[:, [0, 1]] ** 2
         model = PySRRegressor(
-            unary_operators=["sq(x) = x^2"],
-            extra_sympy_mappings={"sq": lambda x: x**2},
+            unary_operators=["square_op(x) = x^2"],
+            extra_sympy_mappings={"square_op": lambda x: x**2},
             binary_operators=["plus"],
             verbosity=0,
             **self.default_test_kwargs,
@@ -49,6 +49,7 @@ class TestPipeline(unittest.TestCase):
         model.fit(self.X, y)
         equations = model.equations
         print(equations)
+        self.assertIn("square_op", equations.sympy())
         self.assertLessEqual(equations[0].iloc[-1]["loss"], 1e-4)
         self.assertLessEqual(equations[1].iloc[-1]["loss"], 1e-4)
 
