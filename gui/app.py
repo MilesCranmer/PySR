edbcfa6f1ac638784c5101bb45959fada90b8ce8
MilesCranmer <miles.cranmer@gmail.com>
2022-06-11 17:19:25 -0400

    Install PyCall.jl with other python instance

--- gui/app.py
+++ gui/app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import os
+
+# Need to install PySR in separate python instance:
+os.system(
+    """
+if [ ! -d "$HOME/.julia/environments/pysr-0.9.1" ]; then
+    python -c 'import pysr; pysr.install()'
+fi
+"""
+)
+import pysr
+from pysr import PySRRegressor
 import numpy as np
 import pandas as pd
 
@@ -13,17 +25,18 @@ if file_name is not None:
     df = pd.read_csv(file_name)
     y = np.array(df["y"])
     X = df.drop(["y"], axis=1)
-    import pysr
 
-    pysr.install()
-    from pysr import PySRRegressor
-
-    model = PySRRegressor()
+    model = PySRRegressor(update=False)
     model.fit(X, y)
 
     col1.header("Equation")
     col2.header("Loss")
-    # model.equations_ is a pd.DataFrame
     for i, row in model.equations_.iterrows():
-        col1.subheader(row["equation"])
-        col2.subheader(row["loss"])
+        col1.subheader(str(row["equation"]))
+        col2.subheader(str(row["loss"]))
+
+    model = None
+
+Main = None
+pysr.sr.Main = None
+pysr.sr.already_ran = False
