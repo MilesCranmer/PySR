f072863ba6b6cf12ba6a0cad1cce3916dd6177ab
MilesCranmer <miles.cranmer@gmail.com>
2022-06-22 12:33:38 -0400

    Working app?

--- gui/app.py
+++ gui/app.py
@@ -2,10 +2,18 @@ import io
 import gradio as gr
 import os
 import tempfile
-from typing import List
 
 
-def greet(file_obj: List[tempfile._TemporaryFileWrapper]):
+def greet(
+    file_obj: tempfile._TemporaryFileWrapper,
+    col_to_fit: str,
+    niterations: int,
+    binary_operators: list,
+    unary_operators: list,
+):
+    if col_to_fit == "":
+        raise ValueError("Please enter a column to predict")
+    niterations = int(niterations)
     # Need to install PySR in separate python instance:
     os.system(
         """if [ ! -d "$HOME/.julia/environments/pysr-0.9.1" ]
@@ -17,26 +25,52 @@ def greet(file_obj: List[tempfile._TemporaryFileWrapper]):
     import numpy as np
     import pandas as pd
 
-    df = pd.read_csv(file_obj[0])
-    # y = np.array(df["y"])
-    # X = df.drop(["y"], axis=1)
+    df = pd.read_csv(file_obj.name)
+    y = np.array(df[col_to_fit])
+    X = df.drop([col_to_fit], axis=1)
 
-    # model = PySRRegressor(update=False, temp_equation_file=True)
-    # model.fit(X, y)
+    model = PySRRegressor(
+        update=False,
+        temp_equation_file=True,
+        niterations=niterations,
+        binary_operators=binary_operators,
+        unary_operators=unary_operators,
+    )
+    model.fit(X, y)
 
-    # df_output = model.equations_
-    df_output = df
-    df_output.to_csv("output.csv", index=False, sep="\t")
-
-    return "output.csv"
+    return model.equations_
 
 
-demo = gr.Interface(
-    fn=greet,
-    description="A demo of PySR",
-    inputs=gr.File(label="Upload a CSV file", file_count=1),
-    outputs=gr.File(label="Equation List"),
-)
-# Add file to the demo:
+def main():
+    demo = gr.Interface(
+        fn=greet,
+        description="A demo of PySR",
+        inputs=[
+            gr.File(label="Upload a CSV file"),
+            gr.Textbox(placeholder="Column to predict"),
+            gr.Slider(
+                minimum=1,
+                maximum=1000,
+                value=40,
+                label="Number of iterations",
+            ),
+            gr.CheckboxGroup(
+                choices=["+", "-", "*", "/", "^"],
+                label="Binary Operators",
+                value=["+", "-", "*", "/"],
+            ),
+            gr.CheckboxGroup(
+                choices=["sin", "cos", "exp", "log"],
+                label="Unary Operators",
+                value=[],
+            ),
+        ],
+        outputs="dataframe",
+    )
+    # Add file to the demo:
 
-demo.launch()
+    demo.launch()
+
+
+if __name__ == "__main__":
+    main()
