222fbf088615854c7213e1f2c597648d17e678c2
MilesCranmer <miles.cranmer@gmail.com>
2022-06-22 11:54:47 -0400

    Init gradio version

--- gui/app.py
+++ gui/app.py
@@ -1,42 +1,42 @@
-import streamlit as st
+import io
+import gradio as gr
 import os
+import tempfile
+from typing import List
 
-# Need to install PySR in separate python instance:
-os.system(
-    """
-if [ ! -d "$HOME/.julia/environments/pysr-0.9.1" ]; then
-    python -c 'import pysr; pysr.install()'
-fi
-"""
+
+def greet(file_obj: List[tempfile._TemporaryFileWrapper]):
+    # Need to install PySR in separate python instance:
+    os.system(
+        """if [ ! -d "$HOME/.julia/environments/pysr-0.9.1" ]
+    then
+        python -c 'import pysr; pysr.install()'
+    fi"""
+    )
+    from pysr import PySRRegressor
+    import numpy as np
+    import pandas as pd
+
+    df = pd.read_csv(file_obj[0])
+    # y = np.array(df["y"])
+    # X = df.drop(["y"], axis=1)
+
+    # model = PySRRegressor(update=False, temp_equation_file=True)
+    # model.fit(X, y)
+
+    # df_output = model.equations_
+    df_output = df
+    df_output.to_csv("output.csv", index=False, sep="\t")
+
+    return "output.csv"
+
+
+demo = gr.Interface(
+    fn=greet,
+    description="A demo of PySR",
+    inputs=gr.File(label="Upload a CSV file", file_count=1),
+    outputs=gr.File(label="Equation List"),
 )
-import pysr
-from pysr import PySRRegressor
-import numpy as np
-import pandas as pd
+# Add file to the demo:
 
-st.title("Interactive PySR")
-file_name = st.file_uploader(
-    "Upload a data file, with your output column labeled 'y'", type=["csv"]
-)
-
-if file_name is not None:
-    col1, col2 = st.columns(2)
-
-    df = pd.read_csv(file_name)
-    y = np.array(df["y"])
-    X = df.drop(["y"], axis=1)
-
-    model = PySRRegressor(update=False)
-    model.fit(X, y)
-
-    col1.header("Equation")
-    col2.header("Loss")
-    for i, row in model.equations_.iterrows():
-        col1.subheader(str(row["equation"]))
-        col2.subheader(str(row["loss"]))
-
-    model = None
-
-Main = None
-pysr.sr.Main = None
-pysr.sr.already_ran = False
+demo.launch()
