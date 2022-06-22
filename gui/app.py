e8191c413b27dced70062033020f175b8361d262
MilesCranmer <miles.cranmer@gmail.com>
2022-06-22 15:41:43 -0400

    Fix issues with not identifying folder

--- gui/app.py
+++ gui/app.py
@@ -41,17 +41,17 @@ def greet(
 
     # Install Julia:
     os.system(
-        """if [ ! -d "~/julia" ]; then
+        """if [ ! -d "/home/user/julia" ]; then
         wget https://julialang-s3.julialang.org/bin/linux/x64/1.7/julia-1.7.3-linux-x86_64.tar.gz
         tar zxvf julia-1.7.3-linux-x86_64.tar.gz
-        mkdir ~/julia
-        mv julia-1.7.3/* ~/julia/
+        mkdir /home/user/julia
+        mv julia-1.7.3/* /home/user/julia/
     fi""")
-    os.environ["PATH"] += ":~/julia/bin/"
+    os.environ["PATH"] += ":/home/user/julia/bin/"
     # Need to install PySR in separate python instance:
     os.system(
-        """if [ ! -d "$HOME/.julia/environments/pysr-0.9.3" ]; then
-        export PATH="$PATH:$HOME/julia/bin/"
+        """if [ ! -d "/home/user/.julia/environments/pysr-0.9.3" ]; then
+        export PATH="$PATH:/home/user/julia/bin/"
         python -c 'import pysr; pysr.install()'
     fi"""
     )
