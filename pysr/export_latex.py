d1f1f2c55551e265911d98c7aebc5a3ae8f56a49
Miles Cranmer <miles.cranmer@gmail.com>
2022-07-04 01:28:13 -0400

    Fix issue with precision of float printing

--- pysr/export_latex.py
+++ pysr/export_latex.py
@@ -6,21 +6,24 @@ from sympy.printing.latex import LatexPrinter
 class PreciseLatexPrinter(LatexPrinter):
     """Modified SymPy printer with custom float precision."""
 
-    def __init__(self, settings=None, prec=3):
+    def __init__(self, settings=None, prec=3, full_prec=True):
         super().__init__(settings)
         self.prec = prec
+        self.full_prec = full_prec
 
     def _print_Float(self, expr):
         # Reduce precision of float:
-        reduced_float = sympy.Float(expr, self.prec)
+        reduced_float = sympy.Float(expr, self.prec, full_prec=self.full_prec)
         return super()._print_Float(reduced_float)
 
 
-def to_latex(expr, prec=3, **settings):
+def to_latex(expr, prec=3, full_prec=True, **settings):
     """Convert sympy expression to LaTeX with custom precision."""
     if len(settings) == 0:
         settings = None
-    printer = PreciseLatexPrinter(settings=settings, prec=prec)
+    printer = PreciseLatexPrinter(
+        settings=settings, prec=prec, full_prec=full_prec
+    )
     return printer.doprint(expr)
 
 
