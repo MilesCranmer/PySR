566aadb7097dfd23f2a8a182daf63bc2fbe9acd6
MilesCranmer <miles.cranmer@gmail.com>
2022-09-02 17:35:36 -0400

    Correctly set up navigation

--- gen_docs.sh
+++ gen_docs.sh
@@ -3,7 +3,7 @@
 cat README.md | grep -v 'pysr_logo.svg' | grep -E -v '\<.*div.*\>' > docs/index.md
 # Transform "## Test status" to "**Test status**":
 cd docs
-sed -i.bak 's/\#\# Test status/**Test status**/g' index.md
+sed -i.bak 's/\#\#\# Test status/**Test status**/g' index.md
 # Change '# ' to '## ':
 sed -i.bak '10,$s/^\# /## /g' index.md
 python generate_papers.py
