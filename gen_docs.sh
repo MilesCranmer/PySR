1b76c0689f550afad28cea06e600ec98377665c0
MilesCranmer <miles.cranmer@gmail.com>
2022-09-02 15:47:07 -0400

    Reduce gen_docs without pydoc-markdown

--- gen_docs.sh
+++ gen_docs.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-cat README.md | grep -v 'pysr_logo.svg' > docs/index.md
-pydoc-markdown --build --site-dir build -vv
-cp docs/build/content/docs/api*.md docs/ && rm -rf docs/build
-for f in docs/api*.md; do mv "$f" "$f.bkup" && cat "$f.bkup" | sed '1,4d' > "$f" && rm "$f.bkup"; done
+#!/bin/bash -e
+
+cat README.md | grep -v 'pysr_logo.svg' | grep -E -v '\<.*div.*\>' > docs/index.md
 cd docs && python generate_papers.py && cd ..
