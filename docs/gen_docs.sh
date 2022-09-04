e5a83c85c1a75acccce3744059e33f446b71d5dd
MilesCranmer <miles.cranmer@gmail.com>
2022-09-04 14:58:49 -0400

    Add streamable to splash page of docs

--- docs/gen_docs.sh
+++ docs/gen_docs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
 # Generate home page using README.md:
-cat ../README.md | grep -v 'pysr_logo.svg' | grep -E -v '\<.*div.*\>' > index.md
+echo '<div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/ncvqhy" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>' > index.md
+cat ../README.md | grep -v 'user-images' | grep -E -v '\<.*div.*\>' >> index.md
 
 # Transform "### Test status" to "**Test status**":
 sed -i.bak 's/\#\#\# Test status/**Test status**/g' index.md
