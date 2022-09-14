9b496542330e5a8e953d9300eb464471cb6383f8
MilesCranmer <miles.cranmer@gmail.com>
2022-09-14 18:12:42 -0400

    Fix example code

--- example.py
+++ example.py
@@ -6,16 +6,20 @@ y = 2.5382 * np.cos(X[:, 3]) + X[:, 0] ** 2 - 0.5
 from pysr import PySRRegressor
 
 model = PySRRegressor(
+    model_selection="best",  # Result is mix of simplicity+accuracy
     niterations=40,
     binary_operators=["+", "*"],
     unary_operators=[
         "cos",
         "exp",
         "sin",
-        "inv(x) = 1/x",  # Custom operator (julia syntax)
+        "inv(x) = 1/x",
+	# ^ Custom operator (julia syntax)
     ],
-    model_selection="best",
-    loss="loss(x, y) = (x - y)^2",  # Custom loss function (julia syntax)
+    extra_sympy_mappings={"inv": lambda x: 1 / x},
+    # ^ Define operator for SymPy as well
+    loss="loss(x, y) = (x - y)^2",
+    # ^ Custom loss function (julia syntax)
 )
 
 model.fit(X, y)
