c3d41aaaa11d999d4238b29955174eedc2a2e7e3
MilesCranmer <miles.cranmer@gmail.com>
2022-11-16 14:48:59 -0500

    Update PySR version

--- gui/install_pysr.sh
+++ gui/install_pysr.sh
@@ -4,11 +4,11 @@ import os
 if [ ! -f "/home/user/.local/bin/julia" ]; then
     wget https://raw.githubusercontent.com/abelsiqueira/jill/main/jill.sh
     chmod a+x jill.sh
-    ./jill.sh --version 1.7.3 -y
+    ./jill.sh --version 1.8.2 -y
 fi
 
 # Need to install PySR in separate python instance:
-if [ ! -d "/home/user/.julia/environments/pysr-0.9.3" ]; then
+if [ ! -d "/home/user/.julia/environments/pysr-0.11.9" ]; then
     export PATH="$PATH:/home/user/julia/bin/"
     python -c 'import pysr; pysr.install()'
 fi
