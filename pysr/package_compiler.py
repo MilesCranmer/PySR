0e576734bfcc981b76d293acc0296a67c76783f1
MilesCranmer <miles.cranmer@gmail.com>
2022-11-28 12:27:21 -0500

    Clean up sysimage creation

--- pysr/package_compiler.py
+++ pysr/package_compiler.py
@@ -7,30 +7,12 @@ from . import PySRRegressor
 from .julia_helpers import init_julia
 
 
-def create_sysimage():
+def create_sysimage(sysimage_name="pysr.so", julia_project=None, quiet=False):
     """Create a PackageCompiler.jl sysimage from a simple PySR run."""
-    sysimage_name = "pysr.so"
 
-    # Example dataset:
-    X = 2 * np.random.randn(100, 5)
-    y = 2.5382 * np.cos(X[:, 3]) + X[:, 0] ** 2 - 0.5
-
-    model = PySRRegressor(
-        unary_operators=["cos"],
-        procs=8,
-        niterations=2,
-        populations=4,
-        verbosity=0,
-        progress=False,
-    )
-    model.fit(X, y)
-
-    from julia import Main
-
+    Main = init_julia(julia_project=julia_project, quiet=quiet)
     cur_project_dir = Main.eval("dirname(Base.active_project())")
+    sysimage_path = str(Path(cur_project_dir) / sysimage_name)
     from julia import PackageCompiler
 
-    PackageCompiler.create_sysimage(
-        ["SymbolicRegression"],
-        sysimage_path=str(Path(cur_project_dir) / sysimage_name),
-    )
+    PackageCompiler.create_sysimage(["SymbolicRegression"], sysimage_path=sysimage_path)
