0a6b4abdba568c217d795b4cfefb60b7eb16d84f
MilesCranmer <miles.cranmer@gmail.com>
2023-03-20 20:27:50 -0400

    Patch scikit-learn to allow complex numbers

---
+++ pysr/sklearn_monkeypatch.py
@@ -0,0 +1,13 @@
+# Here, we monkey patch scikit-learn until this
+# issue is fixed: https://github.com/scikit-learn/scikit-learn/issues/25922
+from sklearn.utils import validation
+
+
+def _ensure_no_complex_data(*args, **kwargs):
+    ...
+
+
+try:
+    validation._ensure_no_complex_data = _ensure_no_complex_data
+except AttributeError:
+    ...
