9941a00c8d86d9d63cdfb4602cddb1b514114aef
--global <boysboothclibborn@gmail.com>
2023-04-20 10:41:36 +0100

    Updated instructions to install Julia dependencies

--- pysr/_cli/main.py
+++ pysr/_cli/main.py
@@ -8,7 +8,7 @@ def cli(context):
     ctx = context
 
 
-@cli.command("install")
+@cli.command("install", help="Install required Julia dependencies")
 @click.option("--julia_project", default=None)
 @click.option("--quiet", default=False)
 @click.option("--precompile", default=None)
