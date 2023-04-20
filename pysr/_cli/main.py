808a4c320cf9f74a17b2d1d5df0dd39285f02c4d
--global <boysboothclibborn@gmail.com>
2023-04-20 15:07:34 +0100

    Implemented PR changes.

--- pysr/_cli/main.py
+++ pysr/_cli/main.py
@@ -8,9 +8,10 @@ def cli(context):
     ctx = context
 
 
-@cli.command("install", help="Install required Julia dependencies")
-@click.option("--julia_project", default=None)
-@click.option("--quiet", default=False)
-@click.option("--precompile", default=None)
+@cli.command("install", help="Install Julia dependencies for PySR")
+@click.option("-p", "--project", default=None, type=str)
+@click.option("-q", "--quiet", is_flag=True, default=False)
+@click.option("--precompile", 'precompile', flag_value=True, default=None)
+@click.option("--no-precompile", 'precompile', flag_value=False, default=None)
 def _install(julia_project, quiet, precompile):
     install(julia_project, quiet, precompile)
