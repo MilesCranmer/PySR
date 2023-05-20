4726b4ad35757b48c74d58537d1c717008162791
MilesCranmer <miles.cranmer@gmail.com>
2023-05-20 21:33:27 +0100

    Refactor CLI unittest with `click.testing`

--- pysr/test/test_cli.py
+++ pysr/test/test_cli.py
@@ -1,57 +1,51 @@
-import subprocess
 import unittest
-
-
-def run_command(command):
-    """
-    Retrieve output of a command string, decode and convert from CRLF to LF formatting
-    """
-    return (
-        subprocess.run(command.split(" "), stdout=subprocess.PIPE)
-        .stdout.decode("utf-8")
-        .replace("\r\n", "\n")
-    )
+from click import testing as click_testing
+from .._cli.main import pysr
 
 
 class TestCli(unittest.TestCase):
-    def test_help_on_all_commands(self):
-        command_to_test = "python -m pysr --help"
-        expected_lines = [
-            "Usage: pysr [OPTIONS] COMMAND [ARGS]...",
-            "",
-            "Options:",
-            "  --help  Show this message and exit.",
-            "",
-            "Commands:",
-            "  install  Install Julia dependencies for PySR.",
-            "",
-        ]
+    # TODO: Include test for custom project here.
+    def setUp(self):
+        self.cli_runner = click_testing.CliRunner()
 
-        expected = "\n".join(expected_lines)
-        actual = run_command(command_to_test)
-        self.assertEqual(expected, actual)
+    def test_help_on_all_commands(self):
+        expected = "\n".join(
+            [
+                "Usage: pysr [OPTIONS] COMMAND [ARGS]...",
+                "",
+                "Options:",
+                "  --help  Show this message and exit.",
+                "",
+                "Commands:",
+                "  install  Install Julia dependencies for PySR.",
+                "",
+            ]
+        )
+        result = self.cli_runner.invoke(pysr, ["--help"])
+        self.assertEqual(expected, result.output)
+        self.assertEqual(0, result.exit_code)
 
     def test_help_on_install(self):
-        command_to_test = "python -m pysr install --help"
-        expected_lines = [
-            "Usage: pysr install [OPTIONS]",
-            "",
-            "  Install Julia dependencies for PySR.",
-            "",
-            "Options:",
-            "  -p, --project PROJECT_DIRECTORY",
-            "                                  Install in a specific Julia project (e.g., a",
-            "                                  local copy of SymbolicRegression.jl).",
-            "  -q, --quiet                     Disable logging.",
-            "  --precompile                    Force precompilation of Julia libraries.",
-            "  --no-precompile                 Disable precompilation.",
-            "  --help                          Show this message and exit.",
-            "",
-        ]
-
-        expected = "\n".join(expected_lines)
-        actual = run_command(command_to_test)
-        self.assertEqual(expected, actual)
+        expected = "\n".join(
+            [
+                "Usage: pysr install [OPTIONS]",
+                "",
+                "  Install Julia dependencies for PySR.",
+                "",
+                "Options:",
+                "  -p, --project PROJECT_DIRECTORY",
+                "                                  Install in a specific Julia project (e.g., a",
+                "                                  local copy of SymbolicRegression.jl).",
+                "  -q, --quiet                     Disable logging.",
+                "  --precompile                    Force precompilation of Julia libraries.",
+                "  --no-precompile                 Disable precompilation.",
+                "  --help                          Show this message and exit.",
+                "",
+            ]
+        )
+        result = self.cli_runner.invoke(pysr, ["install", "--help"])
+        self.assertEqual(expected, result.output)
+        self.assertEqual(0, result.exit_code)
 
 
 def runtests():
