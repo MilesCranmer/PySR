0ddc60ffd77b2c37b410bfad2a52ca4058acd787
MilesCranmer <miles.cranmer@gmail.com>
2023-12-24 05:57:37 +0000

    Specify types for all parameters

--- setup.py
+++ setup.py
@@ -26,5 +26,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 )
