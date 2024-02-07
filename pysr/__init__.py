796a8addf743b3c2c9c4df164dcd2fcaba2d11cf
MilesCranmer <miles.cranmer@gmail.com>
2024-02-07 21:49:59 +0000

    Remove Feynman problems and dataset

--- pysr/__init__.py
+++ pysr/__init__.py
@@ -2,7 +2,6 @@ from . import sklearn_monkeypatch
 from .deprecated import best, best_callable, best_row, best_tex, pysr
 from .export_jax import sympy2jax
 from .export_torch import sympy2torch
-from .feynman_problems import FeynmanProblem, Problem
 from .sr import PySRRegressor, jl
 
 # This file is created by setuptools_scm during the build process:
@@ -12,7 +11,6 @@ __all__ = [
     "sklearn_monkeypatch",
     "sympy2jax",
     "sympy2torch",
-    "FeynmanProblem",
     "Problem",
     "install",
     "PySRRegressor",
