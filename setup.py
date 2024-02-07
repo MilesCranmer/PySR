725c51002a4fb8c7b155e1d3eeed824cb86eea2e
MilesCranmer <miles.cranmer@gmail.com>
2024-02-07 16:36:59 +0000

    Switch to using pyproject.toml for build options

--- setup.py
+++ setup.py
@@ -1,30 +1,6 @@
-import setuptools
-
-try:
-    with open("README.md", "r", encoding="utf8") as fh:
-        long_description = fh.read()
-except FileNotFoundError:
-    long_description = ""
+from setuptools import setup
 
 exec(open("pysr/version.py").read())
 
-setuptools.setup(
-    name="pysr",
-    version=__version__,
-    author="Miles Cranmer",
-    author_email="miles.cranmer@gmail.com",
-    description="Simple and efficient symbolic regression",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/MilesCranmer/pysr",
-    # Read from requirements.txt:
-    install_requires=open("requirements.txt").read().splitlines(),
-    packages=setuptools.find_packages(),
-    package_data={"pysr": ["../Project.toml", "../datasets/*"]},
-    include_package_data=False,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-)
+# Build options are managed in pyproject.toml
+setup()
