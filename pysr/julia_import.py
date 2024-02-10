b7ca126729a7b0efbd7b627867718ea0a03f47ed
MilesCranmer <miles.cranmer@gmail.com>
2024-02-10 15:12:45 +0000

    Only show warnings if juliacall not yet imported

--- pysr/julia_import.py
+++ pysr/julia_import.py
@@ -4,40 +4,32 @@ import warnings
 
 if "juliacall" in sys.modules:
     warnings.warn(
-        "juliacall module already imported. Make sure that you have set `PYTHON_JULIACALL_HANDLE_SIGNALS=yes` to avoid segfaults."
+        "juliacall module already imported. "
+        "Make sure that you have set the environment variable `PYTHON_JULIACALL_HANDLE_SIGNALS=yes` to avoid segfaults. "
+        "Also note that PySR will not be able to configure `JULIA_NUM_THREADS` or `JULIA_OPTIMIZE` for you."
     )
+else:
+    # Required to avoid segfaults (https://juliapy.github.io/PythonCall.jl/dev/faq/)
+    if os.environ.get("PYTHON_JULIACALL_HANDLE_SIGNALS", "yes") != "yes":
+        warnings.warn(
+            "PYTHON_JULIACALL_HANDLE_SIGNALS environment variable is set to something other than 'yes' or ''. "
+            + "You will experience segfaults if running with multithreading."
+        )
 
-# Required to avoid segfaults (https://juliapy.github.io/PythonCall.jl/dev/faq/)
-if os.environ.get("PYTHON_JULIACALL_HANDLE_SIGNALS", "yes") != "yes":
-    warnings.warn(
-        "PYTHON_JULIACALL_HANDLE_SIGNALS environment variable is set to something other than 'yes' or ''. "
-        + "You will experience segfaults if running with multithreading."
-    )
+    if os.environ.get("JULIA_NUM_THREADS", "auto") != "auto":
+        warnings.warn(
+            "JULIA_NUM_THREADS environment variable is set to something other than 'auto', "
+            "so PySR was not able to set it. You may wish to set it to `'auto'` for full use "
+            "of your CPU."
+        )
 
-if os.environ.get("JULIA_NUM_THREADS", "auto") != "auto":
-    warnings.warn(
-        "JULIA_NUM_THREADS environment variable is set to something other than 'auto', "
-        "so PySR was not able to set it. You may wish to set it to `'auto'` for full use "
-        "of your CPU."
-    )
-
-# TODO: Remove these when juliapkg lets you specify this
-for k, default in (
-    ("PYTHON_JULIACALL_HANDLE_SIGNALS", "yes"),
-    ("JULIA_NUM_THREADS", "auto"),
-    ("JULIA_OPTIMIZE", "3"),
-):
-    os.environ[k] = os.environ.get(k, default)
-
-
-def is_in_jupyter() -> bool:
-    return hasattr(__builtins__, "__IPYTHON__")
-
-
-if is_in_jupyter():
-    from IPython import get_ipython
-
-    get_ipython().run_line_magic("load_ext", "juliacall")
+    # TODO: Remove these when juliapkg lets you specify this
+    for k, default in (
+        ("PYTHON_JULIACALL_HANDLE_SIGNALS", "yes"),
+        ("JULIA_NUM_THREADS", "auto"),
+        ("JULIA_OPTIMIZE", "3"),
+    ):
+        os.environ[k] = os.environ.get(k, default)
 
 
 from juliacall import Main as jl  # type: ignore
