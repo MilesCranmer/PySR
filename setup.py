d479b031f409b0a2bcb4de42bfa4251d1fd57fa1
William Thompson <will.thompson@outlook.com>
2024-03-08 09:45:30 -0800

    Apply fixes learned from octofitterpy

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from setuptools import setup
 
@@ -11,16 +12,21 @@ if os.path.exists(".git"):
     }
 else:
     # Read from pyproject.toml directly
-    import re
 
     with open(os.path.join(os.path.dirname(__file__), "pyproject.toml")) as f:
         data = f.read()
         # Find the version
         version = re.search(r'version = "(.*)"', data).group(1)
 
+    with open(os.path.join(os.path.dirname(__file__), "pysr", "juliapkg.json")) as f:
+        data = f.read()
+        # Find the version
+        backend_version = re.search(r'"version":\s+"\W*(.*)"', data).group(1)
+
     # Write the version to version.py
     with open(os.path.join(os.path.dirname(__file__), "pysr", "version.py"), "w") as f:
-        f.write(f'__version__ = "{version}"')
+        f.write(f'__version__ = "{version}"\n')
+        f.write(f'__octofitter_jl_version__ = "{backend_version}"\n')
 
     kwargs = {
         "use_scm_version": False,
@@ -30,3 +36,12 @@ else:
 
 # Build options are managed in pyproject.toml
 setup(**kwargs)
+
+if os.path.exists(".git"):
+    # Write the version to version.py
+    with open(os.path.join(os.path.dirname(__file__), "pysr", "juliapkg.json")) as f:
+        data = f.read()
+        # Find the version
+        backend_version = re.search(r'"version":\s+"\W*(.*)"', data).group(1)
+    with open(os.path.join(os.path.dirname(__file__), "pysr", "version.py"), "a") as f:
+        f.write(f'__octofitter_jl_version__ = "{backend_version}"\n')
