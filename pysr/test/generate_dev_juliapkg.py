7ba43b5eef71b145ae19c685866e589aae5bab15
MilesCranmer <miles.cranmer@gmail.com>
2024-03-24 03:45:30 +0000

    Fix which PySR gets installed in dev test

--- pysr/test/generate_dev_juliapkg.py
+++ pysr/test/generate_dev_juliapkg.py
@@ -9,10 +9,11 @@ path_to_srjl = sys.argv[2]
 with open(juliapkg_json, "r") as f:
     juliapkg = json.load(f)
 
-del juliapkg["packages"]["SymbolicRegression"]["rev"]
-del juliapkg["packages"]["SymbolicRegression"]["url"]
-juliapkg["packages"]["SymbolicRegression"]["path"] = path_to_srjl
-juliapkg["packages"]["SymbolicRegression"]["dev"] = True
+juliapkg["packages"]["SymbolicRegression"] = {
+    "uuid": juliapkg["packages"]["SymbolicRegression"]["uuid"],
+    "path": path_to_srjl,
+    "dev": True,
+}
 
 with open(juliapkg_json, "w") as f:
     json.dump(juliapkg, f, indent=4)
