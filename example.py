a450abbde90b8848a135bf0c2c7fde49ab052a46
wenbang24 <wenbangalt@gmail.com>
2024-07-30 20:32:35 +1000

    made extrapolating when prredicting work :)

--- example.py
+++ example.py
@@ -3,10 +3,10 @@ import numpy as np
 from pysr import PySRSequenceRegressor
 
 X = np.asarray([[1, 2], [3, 4], [4, 6], [7, 10], [11, 16], [18, 26]])
-model = PySRSequenceRegressor(recursive_history_length=2)
+model = PySRSequenceRegressor(recursive_history_length=3)
 model.fit(X)
 
 print(X.shape)
-pred = model.predict(X)
+pred = model.predict(X, extra_predictions=3)
 print(pred)
 print(pred.shape)
