ef26021df4985250feaeb4eea7b7fc8548da740e
wenbang24 <wenbangalt@gmail.com>
2024-08-07 09:00:52 +1000

    updated docstring

--- pysr/regressor_sequence.py
+++ pysr/regressor_sequence.py
@@ -64,7 +64,7 @@ def _check_assertions(
 
 class PySRSequenceRegressor(BaseEstimator):
     """
-    High performance symbolic regression for time series data.
+    High performance symbolic regression for recurrent sequences.
     Based off of the `PySRRegressor` class, but with a preprocessing step for recurrence relations.
 
     Parameters
