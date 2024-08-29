5c360b0ec5d83922bcb496197e87c9d0cc6835fc
wenbang24 <wenbangalt@gmail.com>
2024-08-29 20:14:19 +1000

    updated latex_table docstring to refer to PySRRegressor.latex_table

--- pysr/regressor_sequence.py
+++ pysr/regressor_sequence.py
@@ -277,27 +277,9 @@ class PySRSequenceRegressor(BaseEstimator):
         *args,
         **kwargs,
     ):
-        """Create a LaTeX/booktabs table for all, or some, of the equations.
-
-        Parameters
-        ----------
-        indices : list[int] | list[list[int]]
-            If you wish to select a particular subset of equations from
-            `self.equations_`, give the row numbers here. By default,
-            all equations will be used. If there are multiple output
-            features, then pass a list of lists.
-        precision : int
-            The number of significant figures shown in the LaTeX
-            representations.
-            Default is `3`.
-        columns : list[str]
-            Which columns to include in the table.
-            Default is `["equation", "complexity", "loss", "score"]`.
-
-        Returns
-        -------
-        latex_table_str : str
-            A string that will render a table in LaTeX of the equations.
+        """
+        Generates LaTeX variable names, then creates a LaTeX table of the best equation(s).
+        Refer to `PySRRegressor.latex_table` for information.
         """
         if self.variable_names is not None:
             if len(self.variable_names) == 1:
