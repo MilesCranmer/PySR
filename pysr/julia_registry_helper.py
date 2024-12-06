9332eccf378e17a5c6ec4868b9b00c45279a3695
MilesCranmer <miles.cranmer@gmail.com>
2024-12-06 12:43:23 +0000

    refactor: clean up registry helper code

--- pysr/julia_registry_helper.py
+++ pysr/julia_registry_helper.py
@@ -1,101 +1,70 @@
+"""Utilities for managing Julia registry preferences during package operations."""
+
 import os
-import sys
-
-
-def get_juliaregistrypref_envvarname():
-    # We have this function so that we can define this string in one place, instead of needing
-    # to repeat ourselves in multiple places.
-    name = "JULIA_PKG_SERVER_REGISTRY_PREFERENCE"
-    return name
+import warnings
+from collections.abc import Callable
 
 
 def backup_juliaregistrypref():
-    name = get_juliaregistrypref_envvarname()
-    if name in os.environ:
-        old_value = os.environ["JULIA_PKG_SERVER_REGISTRY_PREFERENCE"]
-        # If the user has set the JULIA_PKG_SERVER_REGISTRY_PREFERENCE environment variable, then
-        # we don't overwrite it.
-        pass
-    else:
-        # If the user has not set the JULIA_PKG_SERVER_REGISTRY_PREFERENCE environment variable, then
-        # we set it temporarily, and unset it when we are done.
-        old_value = None
+    """Backup and potentially modify Julia registry preference.
 
-        # We will set the JULIA_PKG_SERVER_REGISTRY_PREFERENCE to `eager`.
-        # Note: in upstream Julia/Pkg, the default value is `conservative`.
-        # Therefore, by setting it to `eager`, we are overriding the default.
-        # Because we are deviating from the default behavior that would normally be expected in Julia,
-        # I think that it's a good idea to print an informational message, so that the user is aware
-        # of the change being made.
-        info_msg = """\
-            INFO: Attempting to use the `eager` registry flavor of the Julia General registry from the Julia Pkg server.
-            If any errors are encountered, try setting the `{name}` environment variable to `conservative`.
-        """.format(
-            name=name  # TODO: get rid of this line?
+    Sets JULIA_PKG_SERVER_REGISTRY_PREFERENCE to 'eager' if not already set.
+    Returns the original value for later restoration.
+    """
+    old_value = os.environ.get("JULIA_PKG_SERVER_REGISTRY_PREFERENCE", None)
+    if old_value is None:
+        warnings.warn(
+            "Attempting to use the `eager` registry flavor of the Julia "
+            "General registry from the Julia Pkg server.\n"
+            "    If any errors are encountered, try setting the "
+            "`JULIA_PKG_SERVER_REGISTRY_PREFERENCE` environment variable to `conservative`."
         )
-        print(info_msg, file=sys.stderr)
         os.environ["JULIA_PKG_SERVER_REGISTRY_PREFERENCE"] = "eager"
     return old_value
 
 
-def restore_juliaregistrypref(old_value):
-    name = get_juliaregistrypref_envvarname()
+def restore_juliaregistrypref(old_value: str | None):
+    """Restore the original Julia registry preference value."""
     if old_value is None:
-        # Delete the JULIA_PKG_SERVER_REGISTRY_PREFERENCE environment variable that we set:
-        os.environ.pop(name)
+        del os.environ["JULIA_PKG_SERVER_REGISTRY_PREFERENCE"]
     else:
-        # Restore the original value of the JULIA_PKG_SERVER_REGISTRY_PREFERENCE environment variable:
-        os.environ[name] = old_value
-    return None
+        os.environ["JULIA_PKG_SERVER_REGISTRY_PREFERENCE"] = old_value
 
 
-def with_juliaregistrypref(f, *args):
-    name = get_juliaregistrypref_envvarname()
+def with_juliaregistrypref(f: Callable[..., None], *args):
+    """Execute function with modified Julia registry preference.
+
+    Temporarily modifies the registry preference to 'eager', falling back to
+    'conservative' if network errors occur. Restores original preference after
+    execution.
+
+    Parameters
+    ----------
+    f : Callable[..., None]
+        Function to execute. Should not return anything of importance.
+    *args : Any
+        Arguments to pass to the function.
+    """
     old_value = backup_juliaregistrypref()
     try:
         f(*args)
-    except:
-        error_msg = """\
-            ERROR: Encountered a network error.
-            Are you behind a firewall, or are there network restrictions that would prevent access
-            to certain websites or domains?
-            Try setting the `{name}` environment variable to `conservative`.
-        """.format(
-            name=name  # TODO: get rid of this line?
+    except Exception as e:
+        error_msg = (
+            "ERROR: Encountered a network error.\n"
+            "    Are you behind a firewall, or are there network restrictions that would "
+            "prevent access to certain websites or domains?\n"
+            "    Try setting the `JULIA_PKG_SERVER_REGISTRY_PREFERENCE` environment "
+            "variable to `conservative`."
         )
         if old_value is not None:
-            print("", file=sys.stderr)
-            print(error_msg, file=sys.stderr)
-            print("", file=sys.stderr)
-
-            # In this case, we know that the user had set JULIA_PKG_SERVER_REGISTRY_PREFERENCE,
-            # and thus we respect that value, and we don't override it.
-            # So, in this case, we just rethrow the caught exception:
-            restore_juliaregistrypref(
-                old_value
-            )  # Restore the old value BEFORE we re-throw.
-            raise  # Now, re-throw the caught exception.
+            warnings.warn(error_msg)
+            restore_juliaregistrypref(old_value)
+            raise e
         else:
-            # In this case, the user had not set JULIA_PKG_SERVER_REGISTRY_PREFERENCE.
-            # So we had initially tried with JULIA_PKG_SERVER_REGISTRY_PREFERENCE=eager, but that
-            # resulted in an exception.
-            # So let us now try with JULIA_PKG_SERVER_REGISTRY_PREFERENCE=conservative
-            os.environ[name] = "conservative"
+            os.environ["JULIA_PKG_SERVER_REGISTRY_PREFERENCE"] = "conservative"
             try:
-                # Note: after changing the value of `JULIA_PKG_SERVER_REGISTRY_PREFERENCE`,
-                # you need to run `Pkg.Registry.update()` again. Otherwise the change will not take effect.
-                # `juliacall` will automatically do this for us, so we don't need to do it ourselves.
-                #
-                # See line 334 here:
-                # https://github.com/JuliaPy/pyjuliapkg/blob/3a2c66019f098c1ebf84f933a46e7ca70e82792b/src/juliapkg/deps.py#L334-L334
                 f(args)
-            except:
-                print("", file=sys.stderr)
-                print(error_msg, file=sys.stderr)
-                print("", file=sys.stderr)
-                # Now, we just rethrow the caught exception:
-                restore_juliaregistrypref(
-                    old_value
-                )  # Restore the old value BEFORE we re-throw.
-                raise  # Now, re-throw the caught exception.
-    return None
+            except Exception as e:
+                warnings.warn(error_msg)
+                restore_juliaregistrypref(old_value)
+                raise e
