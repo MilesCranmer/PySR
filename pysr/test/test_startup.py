45e45ef474fef85785d41c3bc6b97eea280c4179
MilesCranmer <miles.cranmer@gmail.com>
2026-01-28 20:13:10 +0000

    test: fix nbval env

--- pysr/test/test_startup.py
+++ pysr/test/test_startup.py
@@ -151,6 +151,7 @@ class TestStartup(unittest.TestCase):
                 "-m",
                 "pytest",
                 "--nbval",
+                "--nbval-current-env",
                 str(notebook_file),
                 "--nbval-sanitize-with",
                 str(sanitize_file),
